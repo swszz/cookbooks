201d0c19bba710c57207b3a868c5d2bae3288aed
swszz <swsz@kakao.com>
2023-04-28 23:49:58 +0900

    initialize: 프로젝트 생성

---
+++ kotlin/src/test/kotlin/com/swszz/cookbooks/kotlin/KotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.swszz.cookbooks.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
