39a77055cb634218ba3e9b10499ed006c1450381
swszz <swsz@kakao.com>
2023-04-30 15:54:09 +0900

    docs: 주문 엔티티 주석 추가

--- kopring/src/main/kotlin/com/swszz/kopring/beer/adapter/out/persistence/entity/Order.kt
+++ kopring/src/main/kotlin/com/swszz/kopring/beer/adapter/out/persistence/entity/Order.kt
@@ -9,7 +9,7 @@ import org.hibernate.annotations.Comment
  * Hibernate 는 final class 에 대한 lazy loading 을 위한 proxy 를 생성할 수 없다. <p>
  * Kotlin 에서 모든 클래스는 final class 로 정의되기 때문에, open 예약어를 사용해야 한다. <p>
  * - field 또한 마찬가지다.
- *
+ * 또는 allOpen 플러그인을 활용하는 것도 방법이다.
  * @author: swszz
  */
 @Entity
