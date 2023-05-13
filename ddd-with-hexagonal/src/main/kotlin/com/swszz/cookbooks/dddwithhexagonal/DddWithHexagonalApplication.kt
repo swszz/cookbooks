43ddec1c064bcd492e61e5e2fa5b8a55eff23b79
swszz <swsz@kakao.com>
2023-05-13 14:17:59 +0900

    initialize: ddd with hexagonal arch

---
+++ ddd-with-hexagonal/src/main/kotlin/com/swszz/cookbooks/dddwithhexagonal/DddWithHexagonalApplication.kt
@@ -0,0 +1,11 @@
+package com.swszz.cookbooks.dddwithhexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DddWithHexagonalApplication
+
+fun main(args: Array<String>) {
+    runApplication<DddWithHexagonalApplication>(*args)
+}
