ae229a011e50e7b79b4957260b7b89257b89e45b
swszz <swsz@kakao.com>
2023-05-20 13:58:08 +0900

    initialize: 프로젝트 생성

---
+++ mapping-strategy/src/test/kotlin/com/swszz/cookbooks/mappingstrategy/MappingStrategyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.swszz.cookbooks.mappingstrategy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MappingStrategyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
